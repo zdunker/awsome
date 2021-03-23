6adfc65782d76b7dbe7b454dbd9a63f146e7313a
sheng.li <sheng.li@ihandysoft.com>
2021-03-23 17:06:52 +0800

    add gitignore

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name='awsome',
     packages=['awsome'],
-    version='0.1',
+    version='0.1.1',
     license='MIT',
     description='aws utilities to make my life easier',
     author='zdunker',
     author_email='robinli.forwork@gmail.com',
     url='https://github.com/zdunker/myaws',
-    download_url='https://github.com/zdunker/myaws/archive/refs/tags/0.1.tar.gz',
+    download_url='https://github.com/zdunker/myaws/archive/refs/tags/0.1.1.tar.gz',
     keywords=['aws', 'utility'],
     install_requires=[
         'boto3',
