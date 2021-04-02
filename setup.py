ec5ac154d93c064498a259d68872d9f871397cf9
sheng.li <sheng.li@ihandysoft.com>
2021-04-02 14:03:39 +0800

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
     name='awsome',
     packages=['awsome'],
-    version='0.1.2',
+    version='0.1.3',
     license='MIT',
     description='aws utilities to make my life easier',
     author='zdunker',
